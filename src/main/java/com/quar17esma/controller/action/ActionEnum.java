b715957af3a18eaae41c8aac9352ad566761f211
quar17esma <quar17esma@gmail.com>
2018-05-13 19:09:48 +0300

    Added ActionEnum

---
+++ src/main/java/com/quar17esma/controller/action/ActionEnum.java
@@ -0,0 +1,96 @@
+package com.quar17esma.controller.action;
+
+import com.quar17esma.controller.action.impl.*;
+
+/**
+ * Possible Commands.
+ */
+public enum ActionEnum {
+    LOGIN {
+        {
+            this.command = new Login();
+        }
+    },
+//    LOGOUT {
+//        {
+//            this.command = new Logout();
+//        }
+//    },
+//    REGISTER_CLIENT {
+//        {
+//            this.command = new RegisterClient();
+//        }
+//    },
+//    EDIT_CLIENT {
+//        {
+//            this.command = new EditClient();
+//        }
+//    },
+//    BUY_NOW {
+//        {
+//            this.command = new BuyNow();
+//        }
+//    },
+//    ADD_TO_ORDER {
+//        {
+//            this.command = new AddToOrder();
+//        }
+//    },
+//    SHOW_CART {
+//        {
+//            this.command = new ShowCart();
+//        }
+//    },
+//    MY_ORDERS {
+//        {
+//            this.command = new MyOrders();
+//        }
+//    },
+//    PAY_ORDER {
+//        {
+//            this.command = new PayOrder();
+//        }
+//    },
+//    SEND_ORDER {
+//        {
+//            this.command = new SendOrder();
+//        }
+//    },
+//    SHOW_GOODS {
+//        {
+//            this.command = new ShowGoods();
+//        }
+//    },
+//    DELETE_GOOD {
+//        {
+//            this.command = new DeleteGood();
+//        }
+//    },
+//    EDIT_GOOD {
+//        {
+//            this.command = new EditGood();
+//        }
+//    },
+//    ADD_GOOD {
+//        {
+//            this.command = new AddGood();
+//        }
+//    },
+    CHANGE_LOCALE {
+        {
+            this.command = new ChangeLocale();
+        }
+    },
+//    MANAGE_CLIENTS {
+//        {
+//            this.command = new ManageClients();
+//        }
+//    },
+ ;
+
+    Action command;
+
+    public Action getCurrentCommand() {
+        return command;
+    }
+}
