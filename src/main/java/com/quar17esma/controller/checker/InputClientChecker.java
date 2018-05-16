ca8c39be2e92e76268a18448fdee10c4c7e43146
quar17esma <quar17esma@gmail.com>
2018-05-16 17:00:43 +0300

    Client Data Checker improved with tests

--- src/main/java/com/quar17esma/controller/checker/InputClientChecker.java
+++ src/main/java/com/quar17esma/controller/checker/InputClientChecker.java
@@ -1,18 +1,78 @@
 package com.quar17esma.controller.checker;
 
+import com.quar17esma.enums.Gender;
+import com.quar17esma.enums.Lifestyle;
+
+import java.time.LocalDate;
+
 public class InputClientChecker extends InputDataChecker {
     private static final int NAME_LENGTH_MAX = 100;
     private static final int EMAIL_LENGTH_MAX = 100;
+    private static final int HEIGHT_MIN = 1;
+    private static final int HEIGHT_MAX = 250;
+    private static final int WEIGHT_MIN = 1;
+    private static final int WEIGHT_MAX = 500;
 
-    public boolean isInputDataCorrect(String name, String email) {
+    public boolean isInputDataCorrect(String name, String email, int height, int weight, LocalDate birthDate) {
+        boolean result = false;
 
-        if (name == null || email == null || name.isEmpty() || email.isEmpty()) {
+        if (isNameCorrect(name) &&
+                isEmailCorrect(email) &&
+                isHeightCorrect(height) &&
+                isWeightCorrect(weight) &&
+                isBirthDateCorrect(birthDate)) {
+
+            result = true;
+        }
+
+        return result;
+    }
+
+    private boolean isNameCorrect(String name) {
+        if (name == null || name.isEmpty()) {
             return false;
         }
 
         return isMatches(CheckPatterns.NAME, name) &&
-                isMatches(CheckPatterns.EMAIL, email) &&
-                name.length() <= NAME_LENGTH_MAX &&
+                name.length() <= NAME_LENGTH_MAX;
+    }
+
+    private boolean isEmailCorrect(String email) {
+        if (email == null || email.isEmpty()) {
+            return false;
+        }
+
+        return isMatches(CheckPatterns.EMAIL, email) &&
                 email.length() <= EMAIL_LENGTH_MAX;
+    }
+
+    private boolean isHeightCorrect(int height) {
+        boolean result = false;
+
+        if (height >= HEIGHT_MIN && height <= HEIGHT_MAX) {
+            result = true;
+        }
+
+        return result;
+    }
+
+    private boolean isWeightCorrect(int weight) {
+        boolean result = false;
+
+        if (weight >= WEIGHT_MIN && weight <= WEIGHT_MAX) {
+            result = true;
+        }
+
+        return result;
+    }
+
+    private boolean isBirthDateCorrect(LocalDate birthDate) {
+        boolean result = false;
+
+        if (birthDate.isBefore(LocalDate.now())) {
+            result = true;
+        }
+
+        return result;
     }
 }
--- src/test/java/com/quar17esma/controller/checker/InputClientCheckerTest.java
+++ src/test/java/com/quar17esma/controller/checker/InputClientCheckerTest.java
@@ -4,6 +4,8 @@ import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import java.time.LocalDate;
+
 import static org.mockito.Mockito.*;
 import static org.junit.Assert.*;
 
@@ -13,9 +15,12 @@ public class InputClientCheckerTest {
     public void returnFalseWithNameNull() throws Exception {
         String name = null;
         String email = "some@gmail.com";
+        int height = 182;
+        int weight = 83;
+        LocalDate birthDate = LocalDate.of(1984, 10, 25);
 
         InputClientChecker inputClientChecker = new InputClientChecker();
-        boolean result = inputClientChecker.isInputDataCorrect(name, email);
+        boolean result = inputClientChecker.isInputDataCorrect(name, email, height, weight, birthDate);
 
         assertFalse(result);
     }
@@ -24,9 +29,12 @@ public class InputClientCheckerTest {
     public void returnFalseWithEmailNull() throws Exception {
         String name = "John";
         String email = null;
+        int height = 182;
+        int weight = 83;
+        LocalDate birthDate = LocalDate.of(1984, 10, 25);
 
         InputClientChecker inputClientChecker = new InputClientChecker();
-        boolean result = inputClientChecker.isInputDataCorrect(name, email);
+        boolean result = inputClientChecker.isInputDataCorrect(name, email, height, weight, birthDate);
 
         assertFalse(result);
     }
@@ -35,9 +43,12 @@ public class InputClientCheckerTest {
     public void returnFalseWithNameEmpty() throws Exception {
         String name = "";
         String email = "some@gmail.com";
+        int height = 182;
+        int weight = 83;
+        LocalDate birthDate = LocalDate.of(1984, 10, 25);
 
         InputClientChecker inputClientChecker = new InputClientChecker();
-        boolean result = inputClientChecker.isInputDataCorrect(name, email);
+        boolean result = inputClientChecker.isInputDataCorrect(name, email, height, weight, birthDate);
 
         assertFalse(result);
     }
@@ -46,9 +57,12 @@ public class InputClientCheckerTest {
     public void returnFalseWithEmailEmpty() throws Exception {
         String name = "John";
         String email = "";
+        int height = 182;
+        int weight = 83;
+        LocalDate birthDate = LocalDate.of(1984, 10, 25);
 
         InputClientChecker inputClientChecker = new InputClientChecker();
-        boolean result = inputClientChecker.isInputDataCorrect(name, email);
+        boolean result = inputClientChecker.isInputDataCorrect(name, email, height, weight, birthDate);
 
         assertFalse(result);
     }
@@ -57,9 +71,12 @@ public class InputClientCheckerTest {
     public void returnTrueWithCorrectEmailName() throws Exception {
         String name = "John";
         String email = "john@gmail.com";
+        int height = 182;
+        int weight = 83;
+        LocalDate birthDate = LocalDate.of(1984, 10, 25);
 
         InputClientChecker inputClientChecker = new InputClientChecker();
-        boolean result = inputClientChecker.isInputDataCorrect(name, email);
+        boolean result = inputClientChecker.isInputDataCorrect(name, email, height, weight, birthDate);
 
         assertTrue(result);
     }
@@ -68,9 +85,12 @@ public class InputClientCheckerTest {
     public void returnFalseWithCorrectEmailNameWithNumbers() throws Exception {
         String name = "John123";
         String email = "john@gmail.com";
+        int height = 182;
+        int weight = 83;
+        LocalDate birthDate = LocalDate.of(1984, 10, 25);
 
         InputClientChecker inputClientChecker = new InputClientChecker();
-        boolean result = inputClientChecker.isInputDataCorrect(name, email);
+        boolean result = inputClientChecker.isInputDataCorrect(name, email, height, weight, birthDate);
 
         assertFalse(result);
     }
@@ -79,9 +99,12 @@ public class InputClientCheckerTest {
     public void returnFalseWithCorrectNameWrongEmail() throws Exception {
         String name = "John";
         String email = "johngmail.com";
+        int height = 182;
+        int weight = 83;
+        LocalDate birthDate = LocalDate.of(1984, 10, 25);
 
         InputClientChecker inputClientChecker = new InputClientChecker();
-        boolean result = inputClientChecker.isInputDataCorrect(name, email);
+        boolean result = inputClientChecker.isInputDataCorrect(name, email, height, weight, birthDate);
 
         assertFalse(result);
     }
